bbbcd99c434baadeb3d46633cc6ac02f46afedf4
Isabella Bencardino <isabella@bencardino.com.br>
2022-06-30 13:30:23 +0100

    implement search bar

---
+++ noGlu/HomepageViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class HomepageViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
