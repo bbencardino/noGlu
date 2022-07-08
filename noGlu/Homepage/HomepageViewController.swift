7ab6c1c998ec142bf3be83457b2d3a5844050287
Isabella Bencardino <isabella@bencardino.com.br>
2022-07-08 20:24:15 +0100

    add reachability to the project

--- noGlu/Homepage/HomepageViewController.swift
+++ noGlu/Homepage/HomepageViewController.swift
@@ -5,6 +5,10 @@ class HomepageViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var resultTableView: UITableView!
 
+    @IBOutlet weak var noConnectionView: UIView!
+    @IBOutlet weak var homeView: UIView!
+
+    private let reachability = try? Reachability()
     private let searchBarDelegate = SearchDelegate()
     private let resultDatasource = ResultDataSource()
 
@@ -14,6 +18,42 @@ class HomepageViewController: UIViewController {
         dismissKeyboard()
         searchBar.delegate = searchBarDelegate
         resultTableView.dataSource = resultDatasource
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        setReachability()
+    }
+
+    private func setReachability() {
+        reachability?.whenReachable = { [weak self] _ in
+            self?.showHomeScreen(true)
+        }
+
+        reachability?.whenUnreachable = { [weak self] _ in
+            self?.showHomeScreen(false)
+        }
+
+        NotificationCenter.default
+            .addObserver(self,
+                      selector: #selector(reachabilityChanged),
+                      name: .reachabilityChanged,
+                      object: reachability)
+        do {
+          try reachability?.startNotifier()
+        } catch {
+          print("could not start reachability notifier")
+        }
+    }
+
+    @objc private func reachabilityChanged(_ notification: Notification) {
+        showHomeScreen(reachability?.connection != .unavailable)
+    }
+
+    private func showHomeScreen(_ hasInternet: Bool) {
+        noConnectionView.isHidden = hasInternet
+        homeView.isHidden = !hasInternet
     }
 }
 
